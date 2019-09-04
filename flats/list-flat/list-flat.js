2c4eca392d64535aea7cfd5397d6150985062383
Thủy Vũ Văn <thuyvv@smartosc.com>
2019-09-04 10:48:46 +0700

    update

--- flats/list-flat/list-flat.js
+++ flats/list-flat/list-flat.js
@@ -128,28 +128,91 @@ function validateForm() {
     let selectBlock = document.getElementById('slect-block');
     let inputNumber = document.getElementById('input-number');
     let inputPhone = document.getElementById('input-phone');
+    let inputOwername = document.getElementById('owername');
+    let inputAcreage = document.getElementById('acreage');
+    let inputFlatType = document.getElementById('flat-type');
+    let inputNote = document.getElementById('input-note');
+    let inputNumberNull = document.getElementById('alert-number');
+    let inputBlockNull = document.getElementById('alert-block');
+    let inputAcreageNull = document.getElementById('alert-acreage');
+    let inputPhoneInvalid = document.getElementById('alert-phone-invalid');
     let errorAlert = [];
     if (!inputNumber.value) {
         errorAlert.push("Input number is required")
+        inputNumber.style.border = '1px solid red';
+        inputNumberNull.style.display = 'block';
+        removeError('alert-number')
     }
     if (!selectBlock.value) {
         errorAlert.push("Block is required")
+        selectBlock.style.border = '1px solid red';
+        inputBlockNull.style.display = 'block';
+        removeError('alert-block')
+    }
+    if (!inputAcreage.value) {
+        errorAlert.push("Acreage is required")
+        inputAcreage.style.border = '1px solid red';
+        inputAcreageNull.style.display = 'block';
+        removeError('alert-acreage')
     }
     let isVNPhoneMobile = /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/;
     if (inputPhone.value && !isVNPhoneMobile.test(inputPhone.value)) {
         errorAlert.push('Phone is invalid');
+        inputPhone.style.border = '1px solid red';
+        inputPhoneInvalid.style.display = 'block';
+        removeError('alert-phone-invalid')
     }
     let randomError = 0;
     if (errorAlert.length) {
         randomError = randomNumber(errorAlert.length);
         alert(errorAlert[randomError])
     } else {
-        alert("Success, you can submit");
+        const loadingView = document.getElementById('loading');
+        loadingView.style.display = "block";
+        let data = {
+            block: selectBlock.value,
+            soPhong: inputNumber.value,
+            owerName: inputOwername.value,
+            phone: inputPhone.value,
+            acreage: inputAcreage.value,
+            flatType: inputFlatType.value,
+            note: inputNote.value
+        }
+
+        $("#myModal").trigger("reset");
+        $.ajax({
+            method: 'POST',
+            url: `${API_ENDPOINT}/api/flat/create`,
+            headers: {
+                'Authorization': localStorage.getItem('token'),
+            },
+            data,
+            success: function (result) {
+                loadingView.style.display = "none";
+                if (result.status == 0) {
+                    $("#myModal").modal('hide')
+                    alert("Add flat successfully!");
+                    init();
+                } else {
+                    alert(result.msg)
+                }
+            }
+        })
     }
+}
+
+function removeError(id) {
+    console.log(id);
+    $(`#${id}`).fadeOut(5000);
 }
 
 function randomNumber(to) {
     return Math.floor(Math.random() * to)
+}
+
+function clearForm() {
+    document.getElementById('slect-block').value("");
+    document.getElementById('input-number').value("");
 }
 
 function actionLogout() {
